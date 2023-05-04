9a17aea52f48d8241a8bf27e685be7a68f8550ff
twovife <azizihsan666@gmail.com>
2023-05-04 15:27:46 +0700

    commit build

---
+++ public/build/assets/KepalaMantri-f3b0080e.js
@@ -0,0 +1,1 @@
+import{a}from"./app-b1e3a402.js";const t=()=>a("div",{children:"KepalaMantri"});export{t as default};
---
+++ public/build/assets/RequestPinjaman-7030813f.js
@@ -0,0 +1,1 @@
+import{a}from"./app-b1e3a402.js";const n=()=>a("div",{children:"RequestPinjaman"});export{n as default};

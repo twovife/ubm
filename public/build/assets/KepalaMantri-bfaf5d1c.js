25b314c3c2dfaa44ed1b61c630141a565adfc06e
twovife <azizihsan666@gmail.com>
2023-05-08 09:47:04 +0700

    build

--- public/build/assets/KepalaMantri-bfaf5d1c.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./app-ff24692e.js";const t=()=>a("div",{children:"KepalaMantri"});export{t as default};
---
+++ public/build/assets/KepalaMantri-d026a813.js
@@ -0,0 +1,1 @@
+import{a}from"./app-100f7ec7.js";const t=()=>a("div",{children:"KepalaMantri"});export{t as default};

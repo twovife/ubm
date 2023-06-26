24d77c0e25e7a7070af83b3cf61c9c231672a6e0
twovife <azizihsan666@gmail.com>
2023-06-26 14:01:59 +0700

    test

---
+++ public/build/assets/KepalaMantri-fe9f4b02.js
@@ -0,0 +1,1 @@
+import{a}from"./app-f485e328.js";const t=()=>a("div",{children:"KepalaMantri"});export{t as default};
--- public/build/assets/TablePermintaanDrop-e7c05183.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./app-0a330f65.js";const e=()=>a("div",{children:"TablePermintaanDrop"});export{e as default};
